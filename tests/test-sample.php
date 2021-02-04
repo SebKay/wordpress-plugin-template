e04666d8ee8fa626f27a766e7126683a683ee90d
Seb Kay <seb@sebkay.com>
2021-02-04 19:30:47 +0000

    Installed test scaffolding

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wordpress_Plugin_Template
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
