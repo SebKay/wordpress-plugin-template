676c518b3f5bc8418d76c19dc6cf5f6902aee881
Seb Kay <seb@sebkay.com>
2021-02-04 19:53:37 +0000

    Set up Composer and scripts

---
+++ wordpress-plugin-template.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    Plugin Name: WordPress Plugin Template
+    Description: A simple template for making OOP WordPress plugins.
+    Version: 0.1.0
+    Requires PHP: 7.4
+    Text Domain: wordpress-plugin-template
+    Author: Seb Kay
+    Author URI: http://www.sebkay.com
+*/
