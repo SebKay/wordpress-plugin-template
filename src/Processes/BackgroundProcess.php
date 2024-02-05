e7c8efcf52f9834ff4460de1531f4505445b3a58
Seb Kay <seb@fhoke.com>
2024-02-05 08:26:17 +0000

    Create BackgroundProcess.php

---
+++ src/Processes/BackgroundProcess.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace WPT\Processes;
+
+class BackgroundProcess extends \WPT_WP_Background_Process
+{
+    protected $action = 'wpt_background_process';
+
+    /**
+     * @param  array  $data
+     * @return false
+     */
+    protected function task($data)
+    {
+        try {
+            // Do something
+        } catch (\Exception $e) {
+            \error_log($e);
+
+            \wpt_plugin()->logger()->general()->error($e->getMessage());
+        }
+
+        return false;
+    }
+}
