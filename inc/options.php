31f80341d17c7c066491fe39d2619c0cba208505
Seb Kay <seb@fhoke.com>
2024-02-05 08:14:53 +0000

    Move options set up to Plugin class

--- inc/options.php
+++ inc/options.php
@@ -1,36 +1,65 @@
-<?php
+<?php defined('ABSPATH') or exit; ?>
 
-defined('ABSPATH') or exit;
+<div class="wrap">
+    <h1>
+        <?php echo WPT_PLUGIN_NAME; ?>
+    </h1>
 
-/**
- * Create options page
- */
-function wpt_create_options_page()
-{
-    if (! current_user_can('administrator')) {
-        return;
-    }
+    <form method="post" action="options.php">
+        <?php settings_fields('wpt-options'); ?>
+        <?php do_settings_sections('wpt-options'); ?>
 
-    add_submenu_page(
-        'options-general.php',
-        WPT_PLUGIN_NAME,
-        WPT_PLUGIN_NAME,
-        'manage_options',
-        'wpt-settings',
-        fn () => include WPT_DIR_PATH.'/inc/options-content.php'
-    );
-}
+        <h2 class="title">
+            <?php _e('API', 'wp-plugin-template'); ?>
+        </h2>
 
-add_action('admin_menu', 'wpt_create_options_page');
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">
+                    <?php _e('Text', 'wp-plugin-template'); ?>
+                </th>
+                <td>
+                    <input type="text" name="wpt_text_option" value="<?php echo esc_attr(get_option('wpt_text_option')); ?>">
+                </td>
+            </tr>
 
-/**
- * Register custom options
- */
-function wpt_register_options()
-{
-    register_setting('wpt-options', 'wpt_text_option');
-    register_setting('wpt-options', 'wpt_radio_option');
-    register_setting('wpt-options', 'wpt_select_option');
-}
+            <tr valign="top">
+                <th scope="row">
+                    <?php _e('Radio', 'wp-plugin-template'); ?>
+                </th>
+                <td>
+                    <label>
+                        <input type="radio" name="wpt_radio_option" value="0" <?php checked(0, esc_attr(get_option('wpt_radio_option', 0))); ?>>
+                        <span>
+                            No
+                        </span>
+                    </label>
+                    <br><br>
+                    <label>
+                        <input type="radio" name="wpt_radio_option" value="1" <?php checked(1, esc_attr(get_option('wpt_radio_option', 0))); ?>>
+                        <span>
+                            Yes
+                        </span>
+                    </label>
+                </td>
+            </tr>
 
-add_action('admin_init', 'wpt_register_options');
+            <tr valign="top">
+                <th scope="row">
+                    <?php _e('Select', 'wp-plugin-template'); ?>
+                </th>
+                <td>
+                    <select name="wpt_select_option">
+                        <option value="1" <?php selected(get_option('wpt_select_option'), 1); ?>>1</option>
+                        <option value="10" <?php selected(get_option('wpt_select_option'), 10); ?>>10</option>
+                        <option value="25" <?php selected(get_option('wpt_select_option'), 25); ?>>25</option>
+                    </select>
+                </td>
+            </tr>
+        </table>
+
+        <hr>
+
+        <?php submit_button(); ?>
+    </form>
+</div>
--- src/Plugin.php
+++ src/Plugin.php
@@ -15,10 +15,32 @@ class Plugin
         return $this->logger ??= new Logger();
     }
 
+    public function options(): void
+    {
+        \add_action('admin_menu', function () {
+            if (! \current_user_can('administrator')) {
+                return;
+            }
+
+            \add_submenu_page(
+                'options-general.php',
+                \WPT_PLUGIN_NAME,
+                \WPT_PLUGIN_NAME,
+                'manage_options',
+                'wpt-settings',
+                fn () => include \WPT_DIR_PATH.'/inc/options.php'
+            );
+        }, 10);
+
+        \add_action('admin_init', function () {
+            \register_setting('wpt-options', 'wpt_text_option');
+            \register_setting('wpt-options', 'wpt_radio_option');
+            \register_setting('wpt-options', 'wpt_select_option');
+        }, 10);
+    }
+
     public function run(): void
     {
-        add_action('plugins_loaded', function () {
-            require_once WPT_DIR_PATH.'/inc/options.php';
-        }, 10);
+        \add_action('plugins_loaded', [$this, 'options'], 10);
     }
 }
