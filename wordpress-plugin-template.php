34a17d15c18e74107894dd4271750243e6a9ddcc
Seb Kay <seb@fhoke.com>
2024-02-05 08:07:12 +0000

    Update wordpress-plugin-template.php

--- wordpress-plugin-template.php
+++ wordpress-plugin-template.php
@@ -8,7 +8,7 @@
     Author: Seb Kay
     Author URI: http://sebkay.com
     WC requires at least: 6.0.0
-    WC tested up to: 6.4.1
+    WC tested up to: 6.4.3
 */
 
 defined('ABSPATH') or exit;
@@ -19,11 +19,7 @@ define('WPT_DIR_PATH', plugin_dir_path(__FILE__));
 define('WPT_WP_ROOT', WPT_DIR_PATH.'/../../../');
 define('WPT_LOGS', WPT_DIR_PATH.'/wpt-logs');
 
-if (file_exists(WPT_DIR_PATH.'/vendor/autoload.php')) {
-    require_once WPT_DIR_PATH.'/vendor/autoload.php';
-} else {
-    return;
-}
+require_once WPT_DIR_PATH.'/vendor/autoload.php';
 
 function wpt_plugin()
 {
