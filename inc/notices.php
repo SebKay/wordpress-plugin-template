f0dc4f9910b0824c0cd2b2c119dfc24354f82d87
Seb Kay <seb@fhoke.com>
2024-06-25 10:19:08 +0100

    Create notices.php

---
+++ inc/notices.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace WPT;
+
+use WPT\Enums\NoticeType;
+
+function adminNotices()
+{
+    return [
+        NoticeType::Test->value => [
+            'enabled' => \get_option('wpt_notice_for_'.NoticeType::Test->value, false),
+            'type' => 'info',
+            'text' => __('This is a test notice.', 'wp-plugin-template'),
+        ],
+    ];
+}
+
+function toggleAdminNotice(NoticeType $type, $enabled = true)
+{
+    return \update_option("wpt_notice_for_{$type->value}", $enabled);
+}
+
+function displayAdminNotices()
+{
+    $html = '';
+
+    \collect(adminNotices())
+        ->each(function ($notice) use (&$html) {
+            if ($notice['enabled']) {
+                $html .= <<< EOT
+                    <div class='notice notice-{$notice['type']}'>
+                        <p>{$notice['text']}</p>
+                    </div>
+                EOT;
+            }
+        });
+
+    echo $html;
+}
+
+add_action('admin_notices', 'WPT\displayAdminNotices');
